3d1d96ad6b471585f65f1e22aa28b35810feda86
Vipin <161577828+jangirvipin@users.noreply.github.com>
2024-08-05 12:41:23 +0530

    Update ipad2n2.jsx

--- src/iPad/ipad2n2.jsx
+++ src/iPad/ipad2n2.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 const a="https://www.apple.com/v/ipad/home/ck/images/overview/select/product-tile/pt_ipad_air__cr381zljqdiu_large.png";
 
-
+import { useNavigate } from "react-router-dom";
 export default function Ipad2n2(){
+        const navigate = useNavigate();
     return(
         <div className="ipad2n1">
             <img src={a}></img>
@@ -11,7 +12,7 @@ export default function Ipad2n2(){
             <h3>Serious performance in a thin and</h3>
             <h4>light design.</h4>
             <h2>From $800</h2>
-            <button href="/Store"> Learn more</button>
+            <button navigate=("/Store")> Learn more</button>
             <a href="/BuyIpadAir">Buy</a>
         </div>
     )
